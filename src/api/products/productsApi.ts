5343a7f97dc246685a1c36ef4d105f93ab3c7600
ilya <pasterzoom@gmail.com>
2023-03-06 12:15:47 +0300

    feat: productStore

--- src/api/products/productsApi.ts
+++ src/api/products/productsApi.ts
@@ -1,22 +1,21 @@
-import axios from 'axios'
-
 import { EFilterKeys } from '@/mocks/filters'
+import { productsMock } from '@/mocks/productsMock'
 import { ProductType } from '@/types/productType'
 
 export class ProductsService {
-  BASE_URL = 'http://localhost:3000/api'
+  BASE_URL = ''
 
   getProducts: (filters?: Record<EFilterKeys, string>) => Promise<ProductType[]> =
     async filters => {
-      const res = await axios.get(`${this.BASE_URL}/products`, {
-        params: {
-          assignments: filters?.assignments.length ? filters.assignments : undefined,
-          categories: filters?.categories.length ? filters.categories : undefined,
-          leatherColors: filters?.leatherColors.length ? filters.leatherColors : undefined,
-          leathers: filters?.leathers.length ? filters.leathers : undefined,
-        },
-      })
+      // const res = await axios.get(`${this.BASE_URL}/products`, {
+      //   params: {
+      //     assignments: filters?.assignments.length ? filters.assignments : undefined,
+      //     categories: filters?.categories.length ? filters.categories : undefined,
+      //     leatherColors: filters?.leatherColors.length ? filters.leatherColors : undefined,
+      //     leathers: filters?.leathers.length ? filters.leathers : undefined,
+      //   },
+      // })
 
-      return res.data
+      return productsMock
     }
 }
